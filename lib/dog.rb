01c29eab733d2a33cd8becfa6a57c6ed94c71389
Keanna Salter <keannasalter91@gmail.com>
2020-02-03 19:09:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,10 +2,13 @@ class Dog
 end
 
 fido = Dog.new
-fido #=> #<Dog:0x007fc52c2d7d20>
+fido
+end
  
 snoopy = Dog.new
-snoopy #=> #<Dog:0x007fc52c2d4170>
- 
+snoopy
+end
+
 lassie = Dog.new
-lassie #=> #<Dog:0x007fc52c2cc588>
+lassie
+end
